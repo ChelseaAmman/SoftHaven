ce39e6a21647531f9eaaedac6a24eeca80c0ec27
Mercedes Y Thompson <mythompson@bsu.edu>
2020-04-27 01:55:35 -0400

    set up for post requests and service communication

--- node/TrafficManagementManager.js
+++ node/TrafficManagementManager.js
@@ -27,71 +27,91 @@ server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
 
-var data = [[
-	{
-	  "IMO": "1000000",
-	  "Flag": "Finland",
-	  "Name": "Triton Star",
-	  "Built": "2012",
-	  "CallSign": "null",
-	  "Length": "null",
-	  "Breadth": "null",
-	  "Tonnage": "41101",
-	  "MMSI": "367303060",
-	  "Type": "Bulk Carrier",
-	  "Owner_Code": "null"
-	},
-	{
-	  "IMO": "1000001",
-	  "Flag": "Indonesia",
-	  "Name": "Spob Athamara",
-	  "Built": "null",
-	  "CallSign": "null",
-	  "Length": "47",
-	  "Breadth": "6",
-	  "Tonnage": "null",
-	  "MMSI": "525000003",
-	  "Type": "Product Tanker",
-	  "Owner_Code": "null"
-	},
-	{
-	  "IMO": "1000019",
-	  "Flag": "United Kingdom",
-	  "Name": "Lady K Ii",
-	  "Built": "1961",
-	  "CallSign": "3FOV",
-	  "Length": "57",
-	  "Breadth": "8",
-	  "Tonnage": "551",
-	  "MMSI": "235095435",
-	  "Type": "Yacht",
-	  "Owner_Code": "11715"
-	}
-  ]]
+// var shipdata = [[
+// 	{
+// 	  "IMO": "1000000",
+// 	  "Flag": "Finland",
+// 	  "Name": "Triton Star",
+// 	  "Built": "2012",
+// 	  "CallSign": "null",
+// 	  "Length": "null",
+// 	  "Breadth": "null",
+// 	  "Tonnage": "41101",
+// 	  "MMSI": "367303060",
+// 	  "Type": "Bulk Carrier",
+// 	  "Owner_Code": "null"
+// 	},
+// 	{
+// 	  "IMO": "1000001",
+// 	  "Flag": "Indonesia",
+// 	  "Name": "Spob Athamara",
+// 	  "Built": "null",
+// 	  "CallSign": "null",
+// 	  "Length": "47",
+// 	  "Breadth": "6",
+// 	  "Tonnage": "null",
+// 	  "MMSI": "525000003",
+// 	  "Type": "Product Tanker",
+// 	  "Owner_Code": "null"
+// 	},
+// 	{
+// 	  "IMO": "1000019",
+// 	  "Flag": "United Kingdom",
+// 	  "Name": "Lady K Ii",
+// 	  "Built": "1961",
+// 	  "CallSign": "3FOV",
+// 	  "Length": "57",
+// 	  "Breadth": "8",
+// 	  "Tonnage": "551",
+// 	  "MMSI": "235095435",
+// 	  "Type": "Yacht",
+// 	  "Owner_Code": "11715"
+// 	}
+//   ]]
 
-function getCoordinates(mmsi){
+function sendData(url,data){
+	var xhr = new XMLHttpRequest();
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader('Content-Type', 'json');
+	xhr.send(JSON.stringify({
+    	data
+}));
+}
+
+function getCoordinates(mmsi, url){
 	let C = { "mmsi" : mmsi };
 	// C = notAfunction({ "mmsi" : mmsi });
-	console.log(C);
-	return C;
+	sendData(url,C)
+	return ;
 }
 
-function getEntrance(){
-	return
+function getEntrance(mmsi, url){
+	let E = { "mmsi" : mmsi };
+	// C = notAfunction({ "mmsi" : mmsi });
+	sendData(url,E)
+	return E;
 }
 
-function getDeparture(){
-	return
+function getDeparture(mmsi, url){
+	let D = { "mmsi" : mmsi };
+	// C = notAfunction({ "mmsi" : mmsi });
+	sendData(url,D)
+	return D;
+	
 }
 
-function getMessage(){
-	return
+function getMessage(mmsi, url){
+	let M = { "mmsi" : mmsi };
+	// C = notAfunction({ "mmsi" : mmsi });
+	sendData(url,M)
+	return M;
 }
 
 
-function getShip(mmsi){
+function getShip(mmsi, url){
 	let s = getShip.find( ({ MMSI }) => MMSI === mmsi )
-	return s;
+	sendData(url,s)
+	return;
 }
 
 function getShipData(mmsi, property){
